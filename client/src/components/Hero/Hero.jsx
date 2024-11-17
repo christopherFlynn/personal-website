9f9b212b500cb97822fbfa92c649a01828400328
Christopher Flynn <55606547+christopherFlynn@users.noreply.github.com>
2024-11-17 15:24:27 -0600

    First draft of personal website

---
+++ client/src/components/Hero/Hero.jsx
@@ -0,0 +1,26 @@
+import { getImageUrl } from "../../utils";
+import styles from "./Hero.module.css";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi I&#39;m Christopher</h1>
+        <p className={styles.description}>
+          I&#39;m a full-stack developer with 5 years of experience using React
+          and NodeJS. Reach out if you want to learn more!
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero Image"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
